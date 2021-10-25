660c896fcfdeb59cfbb715b849d6ab481d5a3c0f
fedeth <giornifederico@gmail.com>
2021-10-25 13:21:47 +0200

    fix typo in robot.rb

--- lib/robot_simulator/robot.rb
+++ lib/robot_simulator/robot.rb
@@ -7,7 +7,7 @@ module RobotSimulator
       'NORTH' => [0, 1],
       'EAST' => [1, 0],
       'SOUTH' => [0, -1],
-      'WEST' => [-1, -0]
+      'WEST' => [-1, 0]
     }.freeze
 
     # Internal robot state
