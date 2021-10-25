7e6a4ebd14b31c5b83f2ddb013a55074bf7df70c
Federico Giorni <giornifederico@gmail.com>
2021-10-25 23:26:59 +0200

    fix logic error

--- lib/robot_simulator/tabletop.rb
+++ lib/robot_simulator/tabletop.rb
@@ -13,8 +13,8 @@ module RobotSimulator
     end
 
     def allowed_move?(pos_x, pos_y)
-      valid_x = pos_x.is_a?(Integer) && !pos_x.negative? && pos_x < length
-      valid_y = pos_y.is_a?(Integer) && !pos_y.negative? && pos_y < width
+      valid_x = pos_x.is_a?(Integer) && !pos_x.negative? && pos_x < width
+      valid_y = pos_y.is_a?(Integer) && !pos_y.negative? && pos_y < length
       valid_x && valid_y
     end
   end
